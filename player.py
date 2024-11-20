46092659e3b9ef490ed0ab8c876ebb8d6d4ae990
Lifelimit <norisz234@gmail.com>
2024-11-20 21:11:35 +0200

    Add ability to turn player

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 import pygame
-from constants import PLAYER_RADIUS
+from constants import *
 
 class CircleShape:
     def __init__(self, radius):
@@ -10,6 +10,7 @@ class Player(CircleShape):
         super().__init__(PLAYER_RADIUS)
         self.position = pygame.Vector2(x, y)
         self.rotation = 0
+        
     
     def triangle(self):
         forward = pygame.Vector2(0,1).rotate(self.rotation)
@@ -21,3 +22,14 @@ class Player(CircleShape):
     
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+        
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+            
+    def update(self,dt):
+        keys = pygame.key.get_pressed()
+        
+        if keys[pygame.K_a]:
+            self.rotate(-dt) # rotate left
+        if keys[pygame.K_d]:
+            self.rotate(+dt) # rotate right
