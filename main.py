833795020415e004d7aee43e7f50f2edc509a56e
Lifelimit <norisz234@gmail.com>
2024-11-23 02:07:20 +0200

    Add asteroids, collision and shoot files

--- main.py
+++ main.py
@@ -2,8 +2,12 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import sys
 from constants import *
 from player import Player
+from asteroidfield import AsteroidField
+from asteroids import Asteroid
+from shoot import Shot
 
 def main():
 	pygame.init()
@@ -13,10 +17,16 @@ def main():
  
 	updatable = pygame.sprite.Group()
 	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
+	shots = pygame.sprite.Group()
  
 	Player.containers = (updatable, drawable)
+	Asteroid.containers = (asteroids, updatable, drawable)
+	AsteroidField.containers = (updatable)
+	Shot.containers = (shots, updatable, drawable)
  
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+	asteroidfield = AsteroidField()
 
 	while True:
 		for event in pygame.event.get():
@@ -28,6 +38,11 @@ def main():
 
 		for object in updatable:
 			object.update(dt)
+		for asteroid in asteroids:
+			if asteroid.collission(player):
+				print("Game over!")
+				sys.exit()
+   
 		for object in drawable:
 			object.draw(screen)
 		
